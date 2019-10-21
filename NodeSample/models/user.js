e38b17e161d3d4260e04d077043d2677541cdff4
shrikant <shrikant.sharma@viithiisys.com>
2019-10-21 21:07:26 +0530

    removed unsed variables and required files

--- NodeSample/models/user.js
+++ NodeSample/models/user.js
@@ -1,14 +1,18 @@
-var util = require('util');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var userSchema = new Schema({
-    userName:String,
-    password:String,
-    email:String,
-    create_date: { type: Date, default: Date.now }
+    userName: String,
+    password: String,
+    email: String,
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
 });
 //访问todo对象模型
 mongoose.model('user', userSchema);
-module.exports.Schema =function (modelName){
-    return{model:mongoose.model(modelName)};
+module.exports.Schema = function (modelName) {
+    return {
+        model: mongoose.model(modelName)
+    };
 }
